64c7095e5fd5a1ff9fed6e376415a5e320674425
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-29 18:13:30 -0400

    Added JS folder and script.js file

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+const subBtn = document.getElementById("btn");
+const userInput = document.getElementById("selectLocation");
+const locationSearch = document.getElementById("locationSearch");
+const icon = document.getElementById("icon")
+const locationName = document.getElementById("location");
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+
+subBtn.addEventListener('click', () => {
+  getData();
+});
+
+
+
+function getData () {
+  fetch(`http://api.weatherapi.com/v1/current.json?key=dadde46a779548ae9bc203817222908&q=${userInput.value}&aqi=no
+  `)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    locationSearch.innerHTML = `${data.location.name}`
+    icon.src = `${data.current.condition.icon}`
+    locationName.innerHTML = `${data.location.region}`
+    temp.innerHTML = `${data.current.temp_f}`
+    humidity.innerHTML = `${data.current.humidity}%`
+  })
+}
